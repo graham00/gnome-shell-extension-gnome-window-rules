55f9e20bbea13b8bc3af1772b30277d65d3cbb03
graham00 <graham00@users.noreply.github.com>
2025-07-29 10:21:43 -0400

    removed bundle.sh

--- bundle.sh
+++
@@ -1,5 +0,0 @@
-UUID="gnome-window-rules@graham00.github.com"
-ZIPFILES="extension.js prefs.js metadata.json schemas locale COPYING"
-
-glib-compile-schemas ./schemas/
-zip -qr "$UUID.zip" $ZIPFILES
